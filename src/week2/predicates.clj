984f4c9b16315d7563c3cb73af0a707a6b1638eb
Elmom <elmo.mantynen@iki.fi>
2011-02-01 14:44:13 +0200

    Done predicates.clj, tests pass

--- src/week2/predicates.clj
+++ src/week2/predicates.clj
@@ -1,22 +1,28 @@
 (ns week2.predicates)
 
-(defn generic-doublificate [change-this-name]
-  ":(")
+(defn generic-doublificate [arg]
+  (cond
+    (number? arg) (* 2 arg)
+    (and (coll? arg) (empty? arg)) nil
+    (vector? arg) (map generic-doublificate arg)
+    (list? arg) (map generic-doublificate arg)
+    :else true))
 
 (defn whitespace? [character]
   (Character/isWhitespace character))
 
 (defn empty-string? [string]
-  ":(")
+  (every? whitespace? string))
 
 (defn pred-and [pred1 pred2]
-  identity)
+  #(and (pred1 %) (pred2 %)))
 
 (defn every-book-has-a-title? [books]
-  ":(")
+  (every? :title books))
 
 (defn first-value-for-key [key maps]
-  ":(")
+  (some key maps))
 
 (defn prime? [number]
-  ":(")
+  (let [p #(zero? (mod number %))]
+    (not (some p (range 2 (inc (Math/sqrt number)))))))
