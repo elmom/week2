dd8de5ee5a8461428d554a0f2cc90ab7202a1bb4
Elmom <elmo.mantynen@iki.fi>
2011-02-03 20:07:45 +0200

    'bout half done of collections.clj

--- src/week2/collections.clj
+++ src/week2/collections.clj
@@ -5,35 +5,44 @@
 
 (defn halve [collection]
   (let [middle (/ (count collection) 2)]
-    (split-at middle collection))
+    (split-at middle collection)))
 
 (defn first-five-positives [a-seq]
   (take 5 (take-while pos? a-seq)))
 
 (defn snip [a-seq]
   (let [not-snip? #(not (= % :snip))]
-    (vector (take-while not-snip? a-seq) (rest (drop-while not-snip a-seq)))))
+    (vector (take-while not-snip? a-seq) (rest (drop-while not-snip? a-seq)))))
+
+(defn describe-books [books]
+  (str (str "I have " (count books) ".")
+    (apply str (map #(str " " (:title %) " was written by " (:author %) " .") books))))
 
 (defn monotonic? [a-seq]
-  ":(")
+  (or (apply <= a-seq) (apply >= a-seq)))
 
 (defn transpose [matrix]
   ":(")
 
 (defn exterminate [a-map]
-  ":(")
+  (hash-map (filter #(apply < %) a-map)))
 
 (defn take-3 [a-seq]
-  ":(")
+ (let [[fst snd trd] a-seq]
+   [fst snd trd]))
 
 (defn author-to-string [author]
-  ":(")
+  (let [{name :name
+         birth-year :birth-year
+         death-year :death-year} author]
+    (str name " (" birth-year "-" death-year ")")))
 
-(defn book-to-string [book]
-  ":(")
+(defn book-to-string [{author :author, title :title}]
+    (str "A book, " title ", written by " (author-to-string author)))
 
 (defn my-keys [a-map]
-  ":(")
+  (for [[key] a-map]
+    key))
 
 (defn who-wrote [books index]
   ":(")
@@ -46,5 +55,3 @@
 
 (defn monotonic-prefix [s]
   ":(")
-
-;(
